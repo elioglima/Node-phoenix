e6adab486fdc5350199b35940b9f8f68a66908db
eliolima <elio.lima@grupomalta.com.br>
2019-09-13 17:01:33 -0300

    ajuste

--- phoenix.service.js
+++ phoenix.service.js
@@ -5,7 +5,7 @@ Description=Phoenix API  Http Service
 Environment=NODE_PORT=3000
 Type=simple
 User=ubuntu
-ExecStart=ExecStart=/bin/bash -c '$$(which node) /home/ubuntu/phoenix/app.js'
+ExecStart=/bin/bash -c '$$(which node) /home/ubuntu/phoenix/app.js'
 
 [Install]
 WantedBy=multi-user.target
