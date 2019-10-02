2ff5782b1daf090304b7752e199703b56ef50c5a
eliolima <elio.lima@grupomalta.com.br>
2019-10-02 14:19:43 -0300

    Ajuste de CasaSensitive

--- client/src/pages/logado/Controle/bodys/painel/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default () => {
-    return (
-        <div>
-            <img src={process.env.PUBLIC_URL + "images/temp/historico.svg"} />
-        </div>
-    );
-};
