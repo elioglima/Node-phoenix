5b2c3c73084e1f9f149e5692f6c35f117fe91bfd
eliolima <elio.lima@grupomalta.com.br>
2019-10-02 14:20:44 -0300

    Ajustando UpperCase nome da pasta

---
+++ client/src/pages/logado/Controle/bodys/PAINEL/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default () => {
+    return (
+        <div>
+            <img src={process.env.PUBLIC_URL + "images/temp/historico.svg"} />
+        </div>
+    );
+};
