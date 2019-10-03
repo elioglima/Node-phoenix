8f552e267c0dc7fdcabf36ce5d50635cca464acc
eliolima <elio.lima@grupomalta.com.br>
2019-10-03 19:17:28 -0300

    Ajuste

--- client/src/pages/components/html/DataGrid/index.js
+++ client/src/pages/components/html/DataGrid/index.js
@@ -1,45 +1,57 @@
 import React, { Component } from 'react';
-import { BASE } from './styled'
+import {
+    BASE,
+    Caption,
+    Thead,
+    TheadTR,
+    TheadTrTh,
+    TBody,
+    TBodyTr,
+    TBodyTrTd
+} from './styled'
 
-const Colunas = [{ name: 'Nome' }]
+const Colunas = [
+    { 
+        name: 'nome', 
+        title:'Nome Completo' 
+    },{ 
+        name: 'nome', 
+        title:'Documento' 
+    }
+]
+
+const Rows = [
+    ['Elio Gonçalves de Lima', '216.399.218-77'] 
+]
+
 class index extends Component {
     render() {
         return (
-            <div>
-                <table>
-                    <caption>Lista de Usuários</caption>
-                    <thead>
+            <BASE>
+                <Caption>Lista de Usuários</Caption>
+                <Thead>
+                    {Colunas.map(c => {
+                        return <TheadTrTh>{c.title}</TheadTrTh>
+                    })}
+                </Thead>
+                    <TBody>
+                        { Rows.map(r => {
+                                return (
+                                    <TBodyTr>
+                                        {r.map(c => {
+                                                return <TBodyTrTd>{c}</TBodyTrTd>
+                                        })}
+                                    </TBodyTr>
+                                )
+                         }) }
+                    </TBody>
+                    {/* <tfoot>
                         <tr>
                             <th>Name</th>
                             <th>Instrument</th>
                         </tr>
-                    </thead>
-                    <tfoot>
-                        <tr>
-                            <th>Name</th>
-                            <th>Instrument</th>
-                        </tr>
-                    </tfoot>
-                    <tbody>
-                        <tr>
-                            <td>John Lennon</td>
-                            <td>Rhythm Guitar</td>
-                        </tr>
-                        <tr>
-                            <td>Paul McCartney</td>
-                            <td>Bass</td>
-                        </tr>
-                        <tr>
-                            <td>George Harrison</td>
-                            <td>Lead Guitar</td>
-                        </tr>
-                        <tr>
-                            <td>Ringo Starr</td>
-                            <td>Drums</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                    </tfoot> */}
+            </BASE>
         );
     }
 }
--- client/src/pages/components/html/DataGrid/styled.js
+++ client/src/pages/components/html/DataGrid/styled.js
@@ -5,3 +5,35 @@ export const BASE = styled.div`
     width:100%;
     flex-direction:column;
 `;
+
+export const Caption = styled.div`
+    display: flex;
+    width:100%;
+    flex-direction:column;
+`;
+
+export const Thead = styled.div`
+    display: flex;    
+    flex-direction:column;
+`;
+
+export const TheadTR = styled.tr`
+    display: flex;
+    width:auto;
+`;
+
+export const TheadTrTh = styled.th`
+    display: flex;
+`;
+
+export const TBody = styled.tbody`
+    display: flex;
+`;
+
+export const TBodyTr = styled.tr`
+    display: flex;
+`;
+
+export const TBodyTrTd = styled.tr`
+    display: flex;
+`;
