47cacc342ed2cd05b80a4a1929faa7b464114f02
Elio Gonçalves de Lima <eliolima@MacBook-Pro-de-Elio.local>
2019-10-11 15:28:56 -0300

    Ajuste

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,20 @@
-import { applyMiddleware, createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { connectRouter, routerMiddleware } from "connected-react-router";
+
 import thunk from "redux-thunk";
 import history from "../routes/history";
 
+import sagas from './sagas'
+import createSagaMiddleware from 'redux-saga'
 
 const middlewares = [routerMiddleware(history), thunk];
+const sagaMiddleware = createSagaMiddleware()
+middlewares.push(middlewares)
 
 const store = createStore(
   connectRouter(history)(() => {}),
   applyMiddleware(...middlewares)
 );
 
+sagaMiddleware.run(sagas)
 export default store;
