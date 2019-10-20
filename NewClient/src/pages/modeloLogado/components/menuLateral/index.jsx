ee0b93044a7fd777e8c655ab6b9d42990a2a303d
Elio Gonçalves de Lima <eliolima@MacBook-Pro-de-Elio.local>
2019-10-20 12:55:56 -0300

    incluir perfil

--- NewClient/src/pages/modeloLogado/components/menuLateral/index.jsx
+++ NewClient/src/pages/modeloLogado/components/menuLateral/index.jsx
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { StBase } from './css'
-import MenuItem from './components/menuItem' 
-import MeuPerfil from './components/meuPerfil' 
+import MenuItem from './components/menuItem'
+import MeuPerfil from './components/meuPerfil'
 
 const Objeto = (props) => {
     const [lines, setLines] = useState(props.lines || [])
-    useEffect(() => {        
+    useEffect(() => {
         setLines(props.lines)
     }, [props.lines])
     return (
         <StBase>
-            <MeuPerfil  />
+            <MeuPerfil />
 
-        { lines.map((menu, key) => {
-            return (
-               <div key={'menu'+key}>
-                    <MenuItem {...menu}   />
-               </div>
-            )
-        })
-        }
+            {lines.map((menu, key) => {
+                return (
+                    <div key={'menu' + key}>
+                        <MenuItem {...menu} />
+                    </div>
+                )
+            })
+            }
         </StBase>
     )
 }
